2275f8678f001b7848848334d5f4a80f3c85b357
길길성민 <ksm65501@gmail.com>
2023-07-14 16:33:30 +0900

    fix:주석정리

--- dummy.js
+++ dummy.js
@@ -2,8 +2,9 @@ const createCsvWriter = require("csv-writer").createObjectCsvWriter;
 
 const records = new Array();
 
+//csv 파일 세팅
 const csvWriter = createCsvWriter({
-  path: "./mockData.csv",
+  path: "../mockData.csv",
   header: [
     { id: "flow", title: "Flow" },
     { id: "static", title: "Static" },
@@ -14,6 +15,7 @@ const csvWriter = createCsvWriter({
   ],
 });
 
+//MockData 생성
 const createMuckData = () => {
   let flow = createNum();
   let static = createNum();
@@ -31,15 +33,19 @@ const createMuckData = () => {
   });
 };
 
+//난수 발생
 const createNum = () => {
+  //100이하 소숫점 둘째짜리 난수 발생
   let randomNum = Math.round(Math.random() * 10000) / 100;
   return randomNum;
 };
 
+//MockData 100개 생성
 for (i = 0; i < 100; i++) {
   createMuckData();
 }
 
+//csv 파일 생성
 csvWriter.writeRecords(records).then(() => {
   console.log("Done");
 });
